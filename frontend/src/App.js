8e5b73edcb39d12c9f09d1c1bedce468eb007779
JScheire <jscheire08@gmail.com>
2021-02-19 12:49:30 -0500

    test 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    /*Hello test */
+    /*Hello test 2*/
     return (
         <div className="App">
             <header className="App-header">

73ee61c16ee160b12849bd47e92c6ac0f26d3fea
Riya Tayal <riya@Riyas-MacBook-Pro-2.local>
2021-03-04 17:29:59 -0500

    Updated look of login & registration page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 import '../../styles/Login/Login.scss';
 import { useHistory } from 'react-router-dom';
 
@@ -46,31 +47,38 @@ function Login() {
     };
 
     return (
-        <Form className="login-form">
-            {showError
-                && (
-                    <Alert variant="danger" onClose={() => setShowError(false)} dismissible>
-                        <Alert.Heading>Alert</Alert.Heading>
-                        <ol><li>{error}</li></ol>
-                    </Alert>
-                )}
-            <Form.Group controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Enter username" onChange={(e) => setLoginUsername(e.target.value)} />
-            </Form.Group>
+        <div>
+            <div>
+                <div className="login-form-title">Sign into your account here!</div>
+            </div>
+            <Container>
+                <Form className="login-form">
+                    {showError
+                        && (
+                            <Alert variant="danger" onClose={() => setShowError(false)} dismissible>
+                                <Alert.Heading>Alert</Alert.Heading>
+                                <ol><li>{error}</li></ol>
+                            </Alert>
+                        )}
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control type="username" placeholder="Enter username" onChange={(e) => setLoginUsername(e.target.value)} />
+                    </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)} />
-                <Form.Text className="text-register" onClick={registerAccount}>
-                    {" Don't have an account? Register here. "}
-                </Form.Text>
-            </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password" placeholder="Password" onChange={(e) => setLoginPassword(e.target.value)} />
+                        <Form.Text className="text-register" onClick={registerAccount}>
+                            {" Don't have an account? Register here."}
+                        </Form.Text>
+                    </Form.Group>
 
-            <Button onClick={login}>
-                Submit
-            </Button>
-        </Form>
+                    <Button onClick={login}>
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        </div>
     );
 }
 

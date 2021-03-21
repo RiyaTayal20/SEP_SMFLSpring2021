cf5ce6c522b121c2cbe2251f0eefb60177559b82
Riya Tayal <riya@Riyas-MacBook-Pro-2.local>
2021-03-20 23:02:38 -0400

    removed NavigationBar from Pages.js

--- frontend/src/components/Pages.js
+++ frontend/src/components/Pages.js
@@ -1,11 +1,9 @@
 import RegistrationPage from './Registration/Registration';
 import LoginPage from './Login/Login';
 import HomePage from './Home/Home';
-import NavigationBar from './NavigationBar/NavigationBar';
 
 export {
     RegistrationPage,
     LoginPage,
     HomePage,
-    NavigationBar,
 };

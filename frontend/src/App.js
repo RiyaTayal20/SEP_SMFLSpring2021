ab2c71fc430e614432da6a30c765dbe0d6abd8d1
Jacques <jscheire08@gmail.com>
2021-03-21 23:20:32 -0400

    exclude navbar from login and registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router, Switch, Route,
+} from 'react-router-dom';
 import { RegistrationPage, LoginPage, HomePage } from './components/Pages';
 import Header from './components/Header/Header';
 import NavigationBar from './components/NavigationBar/NavigationBar';
@@ -11,6 +13,7 @@ function App() {
             <div className="App">
                 <Header />
                 <div className="site-container">
+                    { ['/user/register', '/user/login'].indexOf(window.location.pathname) < 0 && <NavigationBar /> }
                     <div className="site-content">
                         <Switch>
                             <Route path="/user/register">
@@ -19,7 +22,6 @@ function App() {
                             <Route path="/user/login">
                                 <LoginPage />
                             </Route>
-                            <NavigationBar />
                             <Route path="/home">
                                 <HomePage />
                             </Route>

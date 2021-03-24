8e1788b75349117d6d1414d9e541b905cd15b00f
aarushipandey <aarushi0824@gmail.com>
2021-03-23 22:32:44 -0400

    New Change in search

--- frontend/src/components/SymbolLookup/SymbolLookup.js
+++ frontend/src/components/SymbolLookup/SymbolLookup.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import '../../styles/SymbolLookup/SymbolLookup.scss';
+import Icon from '../../icons/magnifier.png';
 
 function SymbolLookup() {
     const search = () => { };
     return (
         <div>
             <div className="Symbol-lookup-page">
-                <h1> SYMBOL LOOKUP </h1>
+                <h1 className="symbol-text" style={{ color: '#5367FC', fontSize: '4rem', fontWeight: 'bold' }}>SYMBOL LOOKUP</h1>
                 <Form>
                     <Form.Group className="Search" controlId="Search">
                         <Form.Control type="Search" placeholder="SEARCH" style={{ border: 'none' }} />
+                        <Button className="Search-Button" onClick={search}>
+                            <img src={Icon} alt="magnifier" className="magnifier" />
+                        </Button>
                     </Form.Group>
                 </Form>
-            </div>
-            <div className="Button">
-                <Button className="Search-Button" onClick={search}> </Button>
             </div>
         </div>
     );

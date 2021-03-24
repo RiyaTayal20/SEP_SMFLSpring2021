2e2c9944661676064444c0ffcf8212535a954166
Krishna Prajapati <kjprajapati18@users.noreply.github.com>
2021-03-24 17:52:21 -0400

    fixed eslint issues

--- frontend/src/components/JoinLeague/JoinLeague.js
+++ frontend/src/components/JoinLeague/JoinLeague.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import '../../styles/JoinLeague/JoinLeague.scss';
 import Form from 'react-bootstrap/Form';
@@ -9,117 +8,113 @@ import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
 // import InputGroup from 'react-bootstrap/InputGroup';
 
-function JoinLeague(){
-
+function JoinLeague() {
     const [validated, setValidated] = useState(false);
 
     const handleSubmit = (event) => {
-    const form = event.currentTarget;
+        const form = event.currentTarget;
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
         }
 
-     setValidated(true);
+        setValidated(true);
     };
 
+    return (
+        <div>
+            <div>
+                <div className="join-league-form-title" noValidate validated={validated} onSubmit={handleSubmit} />
+                <Row>
+                    <Col>
+                        <h2 className="order-title">Join a League</h2>
+                    </Col>
+                    <Col sm={2}>
+                        <Button type="submit">Refresh</Button>
+                    </Col>
+                </Row>
+            </div>
 
-return(  
-<div>
-  <div>
-    <div className="join-league-form-title" noValidate validated={validated} onSubmit={handleSubmit}></div>
-      <Row>
-        <Col>
-          <h2 className="order-title">Join a League</h2>
-        </Col>
-        <Col sm={2}>
-          <Button type="submit">Refresh</Button>
-        </Col>
-      </Row>
-   </div>
+            <Form className="join-league-form">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>League Name</th>
+                            <th>End Date</th>
+                            <th>Members</th>
+                            <th> Join</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>League Name 1</td>
+                            <td> 10/21 </td>
+                            <td> 10 </td>
+                            <td>
+                                <Button type="submit">Join League</Button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>League Name 2</td>
+                            <td>3/20</td>
+                            <td> 7 </td>
+                            <td>
+                                <Button type="submit">Join League</Button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>League Name 3</td>
+                            <td>8/24</td>
+                            <td> 12 </td>
+                            <td>
+                                <Button type="submit">Join League</Button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>League Name 4</td>
+                            <td>6/12</td>
+                            <td> 13 </td>
+                            <td>
+                                <Button type="submit">Join League</Button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>League Name 5</td>
+                            <td>9/30</td>
+                            <td> 12 </td>
+                            <td>
+                                <Button type="submit">Join League</Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
 
+                <Form.Group controlId="Private Key">
+                    <Row>
+                        <Form.Label>
+                            Private Key:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="text" placeholder="Enter League Name here..." required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a League Name.
+                            </Form.Control.Feedback>
+                        </Col>
+                        <Col>
+                            <Form.Control type="text" placeholder="Enter private key here" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a Private Key.
+                            </Form.Control.Feedback>
+                        </Col>
+                        <Col>
+                            <Button type="submit">Join League</Button>
+                        </Col>
+                    </Row>
+                </Form.Group>
+            </Form>
+        </div>
 
- <Form className="join-league-form" >
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>League Name</th>
-          <th>End Date</th>
-          <th>Members</th>
-          <th> Join</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>League Name 1</td>
-          <td> 10/21 </td>
-          <td> 10 </td>
-          <td>   
-            <Button type="submit">Join League</Button>
-          </td>
-        </tr>
-        <tr>
-          <td>League Name 2</td>
-          <td>3/20</td>
-          <td> 7 </td>
-          <td> 
-            <Button type="submit">Join League</Button> 
-          </td>
-        </tr>
-        <tr>
-          <td>League Name 3</td>
-          <td>8/24</td>
-          <td> 12 </td>
-          <td>   
-            <Button type="submit">Join League</Button> 
-          </td>
-        </tr>
-        <tr>
-          <td>League Name 4</td>
-          <td>6/12</td>
-          <td> 13 </td>
-          <td>  
-            <Button type="submit">Join League</Button> 
-          </td>
-        </tr>
-        <tr>
-          <td>League Name 5</td>
-          <td>9/30</td>
-          <td> 12 </td>
-          <td>  
-            <Button type="submit">Join League</Button>
-          </td>
-        </tr>
-      </tbody>
-    </Table>
-
-
-<Form.Group controlId="Private Key">
-  <Row>
-    <Form.Label>
-      Private Key:
-    </Form.Label>
-    <Col>
-      <Form.Control type="text" placeholder="Enter League Name here..." required/>
-      <Form.Control.Feedback type="invalid">
-                        Please provide a League Name.
-      </Form.Control.Feedback>
-    </Col>
-    <Col>
-      <Form.Control type="text" placeholder="Enter private key here" required/>
-      <Form.Control.Feedback type="invalid">
-                        Please provide a Private Key.
-      </Form.Control.Feedback>
-    </Col>
-    <Col>
-      <Button type="submit">Join League</Button>
-    </Col>
-  </Row>
-</Form.Group>
-  </Form>
-</div>
-
-   )
+    );
 }
 
 export default JoinLeague;

166059a1c3755be7699c77b284f21402a5146ab4
aarushipandey <aarushi0824@gmail.com>
2021-03-24 12:48:17 -0400

    Final Symbol Lookup Page

--- frontend/src/components/SymbolLookup/SymbolLookup.js
+++ frontend/src/components/SymbolLookup/SymbolLookup.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../../styles/SymbolLookup/SymbolLookup.scss';
-import Icon from '../../icons/magnifier.png';
+import Icon from '../../icons/magnifier-icon.png';
 
 function SymbolLookup() {
     const search = () => { };
     return (
-        <div>
-            <div className="Symbol-lookup-page">
-                <h1 className="symbol-text" style={{ color: '#5367FC', fontSize: '4rem', fontWeight: 'bold' }}>SYMBOL LOOKUP</h1>
-                <Form>
-                    <Form.Group className="Search" controlId="Search">
-                        <Form.Control type="Search" placeholder="SEARCH" style={{ border: 'none' }} />
-                        <Button className="Search-Button" onClick={search}>
-                            <img src={Icon} alt="magnifier" className="magnifier" />
-                        </Button>
+        <div className="symbol-lookup-page">
+            <h1 className="symbol-text" style={{ color: '#5367FC', fontSize: '4rem', fontWeight: 'bold' }}>SYMBOL LOOKUP</h1>
+            <div className="search-container">
+                <Form className="search-component">
+                    <Form.Group className="search" controlId="Search" style={{ backgroundColor: 'Transparent' }}>
+                        <Form.Control type="search" placeholder="SEARCH" style={{ border: '0rem' }} />
                     </Form.Group>
+                    <Button className="search-button" onClick={search} style={{ backgroundColor: 'Transparent', borderColor: 'Transparent' }}>
+                        <div className="image">
+                            <img src={Icon} alt="magnifier" className="magnifier" style={{ width: '30px', height: '30px' }} />
+                        </div>
+                    </Button>
                 </Form>
             </div>
         </div>

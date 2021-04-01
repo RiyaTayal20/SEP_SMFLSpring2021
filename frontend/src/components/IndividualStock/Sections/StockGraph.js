63c09c5186d50bfa78a75b9a0b4104a759b6afab
christinem61 <christine.mathews61@gmail.com>
2021-04-01 16:27:44 -0400

    restructured

---
+++ frontend/src/components/IndividualStock/Sections/StockGraph.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const StockGraph = (props) => {
+    const { graph } = props;
+    const days = Object.keys(graph);
+    const prices = Object.values(graph);
+    return (
+        <div>
+            <Line
+                data={{
+                    labels: days, // all x values
+                    datasets: [
+                        {
+                            label: 'Price',
+                            data: prices, // all y values
+                            borderColor: 'rgba(98, 252, 3, 1)',
+                            hoverBackgroundColor: 'blue',
+                            fill: false,
+                            borderWidth: 1,
+                            lineTension: 0.1,
+                        },
+                    ],
+                }}
+                height={400}
+                width={600}
+                options={{
+                    backgroundColor: 'white',
+                    maintainAspectRatio: false,
+                    tooltips: {
+                        backgroundColor: 'blue',
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                color: 'gray',
+                                zeroLineColor: 'white',
+                            },
+                            scaleLabel: {
+                                display: true,
+                                fontSize: 15,
+                                fontColor: 'white',
+                                fontStyle: 'bold',
+                                labelString: '$',
+                            },
+                            ticks: {
+                                fontColor: 'white',
+                            },
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                color: 'gray',
+                                zeroLineColor: 'white',
+                            },
+                            scaleLabel: {
+                                display: true,
+                                fontSize: 15,
+                                fontColor: 'white',
+                                fontStyle: 'bold',
+                                labelString: 'Days',
+                            },
+                            ticks: {
+                                fontColor: 'white',
+                            },
+                        }],
+                    },
+                    legend: {
+                        labels: {
+                            fontSize: 12,
+                            fontColor: 'white',
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
+};
+
+export default StockGraph;
